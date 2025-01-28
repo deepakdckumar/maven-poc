87c7f2803cd0d6721f06cb557ee08d004f257176
Deepak Choudhary <deepakdckumar1@gmail.com>
2025-01-28 22:28:38 +0530

    First Commit Assembly plugin

---
+++ src/main/java/io/deepakdckumar/TestStringUtils.java
@@ -0,0 +1,9 @@
+package io.deepakdckumar;
+
+import org.apache.commons.lang.StringUtils;
+
+public class TestStringUtils {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.upperCase("deepak"));
+    }
+}
