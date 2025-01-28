91ca2a222b10baed5dac2e11ec1f5ae14c3e5fe1
Deepak Choudhary <deepakdckumar1@gmail.com>
2025-01-28 22:30:19 +0530

    How to run this file

--- src/main/java/io/deepakdckumar/TestStringUtils.java
+++ src/main/java/io/deepakdckumar/TestStringUtils.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang.StringUtils;
 
 public class TestStringUtils {
     public static void main(String[] args) {
+        //java -cp maven-poc-1.0-SNAPSHOT-jar-with-dependencies.jar io.deepakdckumar.TestStringUtils
         System.out.println(StringUtils.upperCase("deepak"));
     }
 }
